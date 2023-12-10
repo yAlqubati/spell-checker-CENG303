c979831b5a6bd32e74d261b2cfe0a2bd13807643
Yousef Saif <y.alqubati56@gmail.com>
2023-12-10 14:14:22 +0300

    a quick message for the commits

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # just for checking if the word is true or false
 # might change the data structure latter to hash or graph
 # still need to find suggestions for the word and calculate the edit distance
+# please if you'll modify the code add comments to explain what you did
+# write a meaningful commit message
 
 from Trie import Trie
 
