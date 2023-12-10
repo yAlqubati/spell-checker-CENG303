1a325c09c4803cc56dada31e4393a252795f8842
Yousef Saif <y.alqubati56@gmail.com>
2023-12-10 14:06:56 +0300

    first commit just check if word is correct or not

---
+++ main.py
@@ -0,0 +1,17 @@
+# just for checking if the word is true or false
+# might change the data structure latter to hash or graph
+# still need to find suggestions for the word and calculate the edit distance
+
+from Trie import Trie
+
+# create a trie
+trie = Trie()
+
+# read the file and insert the words into the trie
+with open('dictionary.txt','r') as f:
+    for line in f:
+        word = line.strip()
+        trie.insert(word)
+
+# read the file and search the words in the trie
+print(trie.search('imagination'))
