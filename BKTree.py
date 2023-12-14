cca0952232500133584c14e30c296d3bebbae3db
ABDULKAREEM MANSOOR QASEM ABDO AL-SARORI <126907306+karim-mansoor@users.noreply.github.com>
2023-12-14 21:21:48 +0300

    Update BKTree.py

---
+++ BKTree.py
@@ -0,0 +1,32 @@
+class BKNode:
+    def __init__(self,word):
+        self.word = word
+        self.children = {}
+
+class BKTree:
+    def __init__(self):
+        self.root = None
+
+    # to insert one word into the tree in a recursive way
+    def insert(self,word):
+
+        # if the tree is empty
+        if self.root is None:
+            self.root = BKNode(word)
+            return
+        
+        else:
+            # use a temp node to traverse the tree
+            temp = self.root
+            # calculate the edit distance between the new word and the root(need to be implemented)
+            distance = 10 # temp value we'll change it later
+
+            #check if that distance is already in the children of the root
+            if distance in temp.children:
+                # if its already there we'll go to that node and insert the word there
+                self.insert(temp.children[distance],word)
+            
+            # if the distance is not in the children of the root we'll add it
+            else:
+                temp.children[distance] = BKNode(word)
+                return
