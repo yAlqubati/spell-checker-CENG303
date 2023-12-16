d788c4ebd52c7f64e415916028ff24484e95e166
Yesodeli <hasanbera0619@gmail.com>
2023-12-16 15:27:42 +0300

    modified search and added binary search to improve preformance

--- BKTree.py
+++ BKTree.py
@@ -33,40 +33,33 @@ class BKTree:
                 break
 
     def search(self, target, max_distance):
-        # If there is no element in the tree return empty list.
         if self.root is None:
             return []
-
+        
         def search_recursive(node, target, max_distance, results):
-        # If the current node is None, return.
             if node is None:
                 return
 
-            # Calculate the Levenshtein distance between the target word and the current node's word.
             distance = levensteinDistance(target, node.word)
-
-            # If the calculated distance is within the allowed max_distance, add the word to the results list.
+            
+            # if the distance is less than or equal to the max distance then we'll add it to the possible results
             if distance <= max_distance:
                 results.append(node.word)
 
-            # Determine the lower and upper bounds for child nodes to visit.
-            lower_bound = distance - max_distance
-            upper_bound = distance + max_distance
+            # we use bisect_left and bisect_right to find the lower and upper bounds of the range of distances
+            # we used binary search to improve the performance
+            lower_bound = bisect_left(node.distances, distance - max_distance)
+            upper_bound = bisect_right(node.distances, distance + max_distance)
 
-            # Iterate through the range from lower_bound to upper_bound.
-            for d in range(lower_bound, upper_bound + 1):
-                # If the current distance 'd' exists in the children of the node,
-                # recursively call this function on the child node.
-                if d in node.children:
-                    search_recursive(node.children[d], target, max_distance, results)
+            # we repeat the search for all the children in the range of distances
+            for i in range(lower_bound, upper_bound):
+                search_recursive(node.children[i], target, max_distance, results)
 
         # Initialize an empty list to store the results.
         results = []
 
         # Start the recursive search from the root of the tree.
         search_recursive(self.root, target, max_distance, results)
-
-        # Return the list of words that stored.
         return results
     
     def wordInTree(self):
